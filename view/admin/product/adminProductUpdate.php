91fe70a2ffdc98f0889bf4fc8b42c468ff2bb437
Dang Ljnh <dangthanhnhatlinh@gmail.com>
2022-12-22 21:57:59 +0700

    fix change image update product

--- view/admin/product/adminProductUpdate.php
+++ view/admin/product/adminProductUpdate.php
@@ -54,10 +54,11 @@ if (!$_SESSION['nameAdmin']) header('Location: ../../../index.php');
   <?php include('../header/headerProduct.php'); ?>
 
   <div class="container">
-    <form action="../../../controller/product/update.php" method="POST" enctype='multipart/form-data' id="form-product-up">
+    <form action="../../../controller/product/update.php" method="POST" enctype='multipart/form-data'
+      id="form-product-up">
       <div class="modal-body row">
-      <input type="hidden" name="id" value="<?= $result->id ?>" />
-      <input type="hidden" name="stockID" value="<?= $result->stockID ?>" />
+        <input type="hidden" name="id" value="<?= $result->id ?>" />
+        <input type="hidden" name="stockID" value="<?= $result->stockID ?>" />
         <div class="form-group col-6">
           <div class="col-sm-12">
             <label for="text">Tên sản phẩm: </label>
@@ -67,20 +68,22 @@ if (!$_SESSION['nameAdmin']) header('Location: ../../../index.php');
         <div class="form-group col-6">
           <div class="col-sm-12">
             <label for="text">Gía: </label>
-            <input type="number" value="<?= $result->price; ?>" name="price" class="form-control" placeholder="Enter price">
+            <input type="number" value="<?= $result->price; ?>" name="price" class="form-control"
+              placeholder="Enter price">
           </div>
         </div>
         <div class="form-group col-6">
           <div class="col-sm-12">
             <label for="text">Cân nặng(Gam): </label>
-            <input type="number" min="0" name="weight" oninput="validity.valid||(value='');" step="0.5" value="<?= $result->weight;?>"
-              class="form-control" placeholder="Enter weight">
+            <input type="number" min="0" name="weight" oninput="validity.valid||(value='');" step="0.5"
+              value="<?= $result->weight; ?>" class="form-control" placeholder="Enter weight">
           </div>
         </div>
         <div class="form-group col-6">
           <div class="col-sm-12">
             <label for="text">Đánh giá(1-5): </label>
-            <input type="number" name="vote" value="<?= $result->vote; ?>" class="form-control" min="1" max="5" placeholder="Enter name">
+            <input type="number" name="vote" value="<?= $result->vote; ?>" class="form-control" min="1" max="5"
+              placeholder="Enter name">
           </div>
         </div>
         <div class="form-group col-6">
@@ -95,19 +98,20 @@ if (!$_SESSION['nameAdmin']) header('Location: ../../../index.php');
         <div class="form-group col-6">
           <div class="col-sm-12">
             <label for="text">Mô tả: </label>
-            <input type="text" name="desc" value="<?= $result->description;?>" class="form-control" placeholder="Enter name">
+            <input type="text" name="desc" value="<?= $result->description; ?>" class="form-control"
+              placeholder="Enter name">
           </div>
         </div>
         <div class="form-group col-6">
           <div class="image-upload">
-          <img src="../../../<?= $result->photoURL; ?>" style="object-fit: cover;" class="imgOld">
+            <img src="../../../<?= $result->photoURL; ?>" style="object-fit: cover;" class="imgOld">
           </div>
         </div>
         <div class="form-group col-8">
           <div class="col-sm-12">
             <label for="text">Danh mục sản phẩm: </label>
-            <select   name="category_product">
-            <option value="0">No Record Found</option>
+            <select name="category_product">
+              <option value="0">No Record Found</option>
               <?php
               $query = "SELECT * FROM category";
               $statement = $conn->prepare($query);
@@ -117,7 +121,7 @@ if (!$_SESSION['nameAdmin']) header('Location: ../../../index.php');
               if ($result) {
                 foreach ($result as $row) {
               ?>
-              <option value="<?= $row->categoryID; ?>" ><?= $row->name; ?></option>
+              <option value="<?= $row->categoryID; ?>"><?= $row->name; ?></option>
               <?php
                 }
               } else {
@@ -147,13 +151,13 @@ if (!$_SESSION['nameAdmin']) header('Location: ../../../index.php');
   <script src="../../../js/jquery.slicknav.js"></script>
   <script src="../../../js/mixitup.min.js"></script>
   <script src="../../../js/owl.carousel.min.js"></script> -->
-  <script src="http://code.jquery.com/jquery-3.4.1.min.js" 
+  <script src="http://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script type="text/javascript"
-    src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.js">
+  </script>
   <script src="../../../js/jquery-checkUpProduct.js"></script>
-  
- 
+
+
 
 
 
@@ -161,6 +165,7 @@ if (!$_SESSION['nameAdmin']) header('Location: ../../../index.php');
   const fileImage = document.querySelector(".file-image")
   const imageUpload = document.querySelector(".image-upload")
   const labelFile = document.querySelector(".custom-file-label")
+  labelFile.textContent = labelFile.textContent.slice(10)
   fileImage.addEventListener("change", (e) => handleChangeFile(e))
   const toBase64 = (file) => {
     return new Promise((resolve, reject) => {
@@ -171,10 +176,12 @@ if (!$_SESSION['nameAdmin']) header('Location: ../../../index.php');
     });
   };
   const handleChangeFile = async (e) => {
+    const imgOld = document.querySelector(".imgOld")
     if (e?.target && e?.target?.files[0]) {
       labelFile.textContent = e?.target?.files[0].name
       let strBase64 = await toBase64(e.target.files[0]);
-      if (!imageUpload.innerHTML) {
+      if (imgOld) {
+        imgOld.parentNode.removeChild(imgOld)
         imageUpload.innerHTML += `
           <img
           src=data:image/jpeg;base64${strBase64}
@@ -193,6 +200,7 @@ if (!$_SESSION['nameAdmin']) header('Location: ../../../index.php');
           />
           `
       }
+
     }
   };
   </script>
